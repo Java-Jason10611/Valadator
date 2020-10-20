1aa752e79cbd37b1a82b65415dfff5bc96260bfc
r0flcopt0rz <r0flcopt0rz10611@gmail.com>
2020-10-20 12:49:16 -0400

    added yes no validation,put the data collection in its own loop to allow user to re enter data if its not right after they review it at the end

--- Program.cs
+++ Program.cs
@@ -13,47 +13,59 @@ namespace Valadator
             string email = string.Empty;
             string date = string.Empty;
             string html = string.Empty;
-            Console.WriteLine("Please enter a name");
+            string yesNo = string.Empty;
             do
             {
-                name = Console.ReadLine();
-            } 
-            while (!NameValadator(name));
+                Console.WriteLine("Please enter a name");
+          
+                do
+                {
+                    name = Console.ReadLine();
+                }
+                while (!NameValidator(name));
 
-            Console.WriteLine("Please enter a  phone number");
-            do
-            {
-               phoneNumber = Console.ReadLine();
+                Console.WriteLine("Please enter a  phone number");
+                do
+                {
+                    phoneNumber = Console.ReadLine();
+                }
+                while (!PhoneNumberValidator(phoneNumber));
+
+                Console.WriteLine("Please enter a  email");
+                do
+                {
+                    email = Console.ReadLine();
+                }
+                while (!EmailValidator(email));
+
+                Console.WriteLine("Please enter a  date");
+                do
+                {
+                    date = Console.ReadLine();
+                }
+                while (!DateValidator(date));
+                Console.WriteLine("Please enter an html");
+                do
+                {
+                    html = Console.ReadLine();
+                }
+                while (!HtmlValidator(html));
+
+                Console.WriteLine("The info you habe entered is:");
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"Phone number: {phoneNumber}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Date: {date}");
+                Console.WriteLine($"HTML: {html}");
+                Console.WriteLine("\n");
+                Console.WriteLine($"Is this info correct?");
+                Console.WriteLine($"Plese enter yes or no");
+                yesNo = Console.ReadLine();
             }
-            while (!PhoneNumberValadator(phoneNumber));
-
-            Console.WriteLine("Please enter a  email");
-            do
-            {
-                email = Console.ReadLine();
-            } 
-            while (!EmailValadator(email));
-
-            Console.WriteLine("Please enter a  date");
-            do
-            {
-                date = Console.ReadLine();
-            } 
-            while (!DateValadator(date));
-            do
-            {
-                html = Console.ReadLine();
-            }
-            while (!HtmlValadator(html));
-
-            Console.WriteLine("The info you habe entered is:");
-            Console.WriteLine($"Name: {name}");
-            Console.WriteLine($"Phone number: {phoneNumber}");
-            Console.WriteLine($"Email: {email}");
-            Console.WriteLine($"Date: {date}");
-            Console.WriteLine($"HTML: {html}");
+            while (!YesNoValidator(yesNo));
+            Console.WriteLine("thank you for submitting your data using my validation program!");
         }
-        private static bool NameValadator(string name)
+        private static bool NameValidator(string name)
         {
             Regex namePattern = new Regex(@"^[A-Z]{1}[a-z]{1,29}");
 
@@ -65,7 +77,7 @@ namespace Valadator
             return false;
         }
 
-        private static bool PhoneNumberValadator(string phoneNumber)
+        private static bool PhoneNumberValidator(string phoneNumber)
         {
             Regex phoneNumberPattern = new Regex(@"^((\d{3}-)|(\(\d{3}\)(-| )))?\d{3}-\d{4}$");
 
@@ -77,7 +89,7 @@ namespace Valadator
             return false;
         }
       
-    private static bool EmailValadator(string email)
+    private static bool EmailValidator(string email)
         {
             Regex emailPattern = new Regex(@"^[A-z,0-9]{5,30}@[A-z,0-9]{5,10}.com");
 
@@ -89,7 +101,7 @@ namespace Valadator
             return false;
         }
 
-        private static bool DateValadator(string date)
+        private static bool DateValidator(string date)
         {
             Regex datePattern = new Regex(@"^[0-9]{2}/[0-9]{2}/[0-9]{4}");
 
@@ -102,7 +114,7 @@ namespace Valadator
             return false;
             
         }
-       private static bool HtmlValadator(string html)
+       private static bool HtmlValidator(string html)
        {
             Regex htmlTagPattern = new Regex(@"^<[a-z]+>.+?<\/[a-z]+>$");
             if (htmlTagPattern.IsMatch(html))
@@ -112,5 +124,16 @@ namespace Valadator
             Console.WriteLine("HTML format not valid, please enter a valid HTML");
             return false;
        }
+        private static Boolean YesNoValidator(string yesNo)
+        {
+            Regex yesNoPattern = new Regex(@"^((Y|y)|(ES|es))");
+            if (yesNoPattern.IsMatch(yesNo))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    
     }
 }
