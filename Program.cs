ebf1381dab32d2a5f8145a889f426e3b17c9818f
r0flcopt0rz <r0flcopt0rz10611@gmail.com>
2020-10-22 18:29:34 -0400

    changed methods a bit

--- Program.cs
+++ Program.cs
@@ -16,42 +16,70 @@ namespace Valadator
             string yesNo = string.Empty;
             do
             {
-                Console.WriteLine("Please enter a name");
-          
+                Console.WriteLine("Please enter a name");          
                 do
                 {
                     name = Console.ReadLine();
+                    if (NameValidator(name))
+                    {
+                        Console.WriteLine("thank you for a vailid input");
+                        continue;
+                    }
+                    Console.WriteLine("Name format not valid, please enter a valid name");
+                    
                 }
                 while (!NameValidator(name));
-
                 Console.WriteLine("Please enter a  phone number");
                 do
                 {
                     phoneNumber = Console.ReadLine();
+                    if (!PhoneNumberValidator(phoneNumber))
+                    {
+                        Console.WriteLine("thank you for a vailid input");
+                        continue;
+                    }
+                    Console.WriteLine("Phone number format not valid, please enter a valid phone number");
+                    
                 }
                 while (!PhoneNumberValidator(phoneNumber));
-
                 Console.WriteLine("Please enter a  email");
                 do
                 {
                     email = Console.ReadLine();
+                    if (!EmailValidator(email))
+                    {
+                        Console.WriteLine("thank you for a vailid input");
+                        continue;
+                    }
+                    Console.WriteLine("Email format not valid, please enter a valid email");
                 }
                 while (!EmailValidator(email));
-
                 Console.WriteLine("Please enter a  date");
                 do
                 {
                     date = Console.ReadLine();
+                    if (!DateValidator(date))
+                    {
+                        Console.WriteLine("thank you for a vailid input");
+                        continue;
+                    }
+                    Console.WriteLine("Date format not valid, please enter a valid date");
                 }
                 while (!DateValidator(date));
                 Console.WriteLine("Please enter an html");
                 do
                 {
                     html = Console.ReadLine();
+                    if (!HtmlValidator(html))
+                    {
+                        Console.WriteLine("thank you for a vailid input");
+                        continue;
+                    }
+                    Console.WriteLine("HTML format not valid, please enter a valid HTML");
                 }
                 while (!HtmlValidator(html));
 
-                Console.WriteLine("The info you habe entered is:");
+                Console.WriteLine("The info you have entered is:");
                 Console.WriteLine($"Name: {name}");
                 Console.WriteLine($"Phone number: {phoneNumber}");
                 Console.WriteLine($"Email: {email}");
@@ -61,7 +89,7 @@ namespace Valadator
                 Console.WriteLine($"Is this info correct?");
                 Console.WriteLine($"Plese enter yes or no");
                 yesNo = Console.ReadLine();
-            }
+            }  
             while (!YesNoValidator(yesNo));
             Console.WriteLine("thank you for submitting your data using my validation program!");
         }
@@ -69,61 +97,36 @@ namespace Valadator
         {
             Regex namePattern = new Regex(@"^[A-Z]{1}[a-z]{1,29}");
 
-            if (namePattern.IsMatch(name)) 
-            {
-                return true;
-            }
-            Console.WriteLine("Name format not valid, please enter a valid name");
-            return false;
+            return namePattern.IsMatch(name); 
         }
 
         private static bool PhoneNumberValidator(string phoneNumber)
         {
             Regex phoneNumberPattern = new Regex(@"^((\d{3}-)|(\(\d{3}\)(-| )))?\d{3}-\d{4}$");
 
-            if (phoneNumberPattern.IsMatch(phoneNumber)) 
-            {
-                return true;
-            }
-            Console.WriteLine("Phone number format not valid, please enter a valid phone number");
-            return false;
+            return (phoneNumberPattern.IsMatch(phoneNumber)); 
         }
-      
-    private static bool EmailValidator(string email)
+        
+        private static bool EmailValidator(string email)
         {
             Regex emailPattern = new Regex(@"^[A-z,0-9]{5,30}@[A-z,0-9]{5,10}.com");
 
-            if (emailPattern.IsMatch(email))
-            {
-                return true;
-            }
-            Console.WriteLine("Email format not valid, please enter a valid email");
-            return false;
+            return (emailPattern.IsMatch(email));
         }
 
         private static bool DateValidator(string date)
         {
-            Regex datePattern = new Regex(@"^[0-9]{2}/[0-9]{2}/[0-9]{4}");
+            Regex datePattern = new Regex(@"^[0-9]{2}/[0-9]{2}/[0-9]{4}$");
 
-            if (datePattern.IsMatch(date))
-            {
-                return true;
-
-            }
-            Console.WriteLine("Date format not valid, please enter a valid date");
-            return false;
-            
+            return (datePattern.IsMatch(date));
         }
-       private static bool HtmlValidator(string html)
+
+        private static bool HtmlValidator(string html)
        {
             Regex htmlTagPattern = new Regex(@"^<[a-z]+>.+?<\/[a-z]+>$");
-            if (htmlTagPattern.IsMatch(html))
-            {
-                return true;
-            }
-            Console.WriteLine("HTML format not valid, please enter a valid HTML");
-            return false;
+            return (htmlTagPattern.IsMatch(html));       
        }
+
         private static Boolean YesNoValidator(string yesNo)
         {
             Regex yesNoPattern = new Regex(@"^((Y|y)|(ES|es))");
@@ -132,8 +135,6 @@ namespace Valadator
                 return true;
             }
             return false;
-        }
-
-    
+        } 
     }
 }
